a695203f388d536929e102228aa8042ffcb8ba8f
ESNEIDER MUNERA TABARES <esneidermunera1158132@correo.itm.edu.co>
2026-02-26 12:20:50 -0500

    Complete

--- LogicConceptsEMT/Shared/ConsoleExtension.cs
+++ LogicConceptsEMT/Shared/ConsoleExtension.cs
@@ -25,4 +25,36 @@ public class ConsoleExtension
     }
 
 
+    }
+    public static float GetFloat(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (float.TryParse(numberString, out float numberFloat))
+        {
+            return numberFloat;
+        }
+
+        return 0;
+    }
+
+    public static decimal GetDecimal(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (decimal.TryParse(numberString, out decimal numberDecimal))
+        {
+            return numberDecimal;
+        }
+
+        return 0;
+    }
+
+
+    public static string? GetString(string message)
+    {
+        Console.Write(message);
+        var text = Console.ReadLine();
+        return text;
+    }
 }
